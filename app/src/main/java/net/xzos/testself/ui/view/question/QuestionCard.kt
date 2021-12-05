992836b81c6f78c00cbb6f92707fab7692e0b270
xz-dev <xiangzhedev@gmail.com>
2021-12-05 12:44:38 +0800

    fix: 选项显示不完全

--- app/src/main/java/net/xzos/testself/ui/view/question/QuestionCard.kt
+++ app/src/main/java/net/xzos/testself/ui/view/question/QuestionCard.kt
@@ -1,10 +1,7 @@
 package net.xzos.testself.ui.view.question
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -31,7 +28,7 @@ fun ChoiceList(viewModel: QuestionViewModel) {
         viewModel.question.option.forEach { (num, text) ->
             Row(
                 modifier = Modifier
-                    .height(30.dp)
+                    .defaultMinSize(minHeight = 30.dp)
                     .clickable { viewModel.setChoice(num) },
                 verticalAlignment = Alignment.CenterVertically,
             ) {
