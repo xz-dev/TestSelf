9abc4dc54079dd9990dde40023f2d3caf1561c5c
xz-dev <xiangzhedev@gmail.com>
2021-12-05 12:33:35 +0800

    ci: 修改应用名称

--- app/src/main/java/net/xzos/testself/ui/view/About.kt
+++ app/src/main/java/net/xzos/testself/ui/view/About.kt
@@ -10,7 +10,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.withStyle
@@ -18,13 +18,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import net.xzos.testself.BuildConfig
+import net.xzos.testself.R
 import net.xzos.testself.ui.theme.TextColorLight
 import net.xzos.testself.ui.theme.TextColorRed
 
 @Preview
 @Composable
 fun About() {
-    val content = LocalContext.current
     var powerTextName by remember { mutableStateOf("xz") }
     var powerTextColor by remember { mutableStateOf(Color.Unspecified) }
     Box(
@@ -34,7 +34,7 @@ fun About() {
     ) {
         Text(
             modifier = Modifier.align(Alignment.TopCenter),
-            text = "Citalopram",
+            text = stringResource(R.string.app_name),
             fontSize = 30.sp
         )
         Column(
