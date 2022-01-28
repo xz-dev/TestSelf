8532b234e3b7113cd130371a4e16c487a084f401
xz-dev <xiangzhedev@gmail.com>
2022-01-28 11:36:33 +0800

    ci: 更新依赖

--- app/src/main/java/net/xzos/testself/ui/view/About.kt
+++ app/src/main/java/net/xzos/testself/ui/view/About.kt
@@ -1,5 +1,6 @@
 package net.xzos.testself.ui.view
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,6 +11,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.buildAnnotatedString
@@ -25,18 +27,24 @@ import net.xzos.testself.ui.theme.TextColorRed
 @Preview
 @Composable
 fun About() {
-    var powerTextName by remember { mutableStateOf("xz") }
-    var powerTextColor by remember { mutableStateOf(Color.Unspecified) }
     Box(
         modifier = Modifier
             .fillMaxSize()
             .padding(30.dp)
     ) {
-        Text(
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.align(Alignment.TopCenter),
-            text = stringResource(R.string.app_name),
-            fontSize = 30.sp
-        )
+        ) {
+            Image(
+                painterResource(R.drawable.ic_launcher_foreground),
+                "App Logo",
+            )
+            Text(
+                text = stringResource(R.string.app_name),
+                fontSize = 30.sp,
+            )
+        }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier.align(Alignment.BottomCenter),
@@ -45,6 +53,8 @@ fun About() {
                 text = "version: " + BuildConfig.VERSION_NAME,
                 color = TextColorLight,
             )
+            var powerTextName by remember { mutableStateOf("xz") }
+            var powerTextColor by remember { mutableStateOf(Color.Unspecified) }
             Text(
                 color = TextColorLight,
                 modifier = Modifier
